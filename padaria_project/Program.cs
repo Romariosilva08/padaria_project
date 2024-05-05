9ed3642e9fa47ee35e27b0fa4e90df4551f22b1a
Romário <Romrio@DESKTOP-T6S5O68>
2024-05-05 19:37:54 -0300

    alterando rotas da Program

--- padaria_project/Program.cs
+++ padaria_project/Program.cs
@@ -1,15 +1,45 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddRazorPages();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapRazorPages();
+
+//app.Run();
+
+
+
+
+
+//IMPLEMENTADO POR ULTIMO
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adiciona serviços ao contêiner.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure o pipeline de solicitação HTTP.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,7 +52,13 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
+// Configura explicitamente a rota padrão para "https://localhost:7039/cadastro.html"
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGet("/", async context =>
+    {
+        context.Response.Redirect("/cadastro.html");
+    });
+});
+
 app.Run();
-
-
-
